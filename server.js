2731b614b61697be97b4700eb75fbfe41eb23bc1
Tom <codertomo@gmail.com>
2014-07-07 18:41:10 -0700

    Changed port.

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var express = require("express");           // web framework external module
 var io      = require("socket.io");         // web socket external module
 var easyrtc = require("easyrtc");           // EasyRTC external module
 
+var port = process.env.PORT || 8080;
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
 httpApp.use(express.static(__dirname + "/static/"));
 
 // Start Express http server on port 8080
-var webServer = http.createServer(httpApp).listen(8080);
+var webServer = http.createServer(httpApp).listen(port);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
