73d21702587d8f637c02f4355d89f506717a28a1
Tom <codertomo@gmail.com>
2014-07-08 11:00:03 -0700

    Added styling and jquery.

---
+++ static/app.js
@@ -0,0 +1,64 @@
+var selfEasyrtcid = "";
+
+var connect = function () {
+  easyrtc.setRoomOccupantListener(convertListToButtons);
+  easyrtc.easyApp("easyrtc.audioVideo", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
+};
+
+var clearConnectList = function () {
+  var otherClientDiv = document.getElementById('otherClients');
+  while (otherClientDiv.hasChildNodes()) {
+    otherClientDiv.removeChild(otherClientDiv.lastChild);
+  }
+};
+
+var convertListToButtons = function (roomName, data, isPrimary) {
+  clearConnectList();
+  var otherClientDiv = document.getElementById('otherClients');
+  for (var easyrtcid in data) {
+    var button = document.createElement('button');
+    button.onclick = function (easyrtcid) {
+      return function () {
+        performCall(easyrtcid);
+      };
+    }(easyrtcid);
+
+    var label = document.createTextNode(easyrtc.idToName(easyrtcid));
+    button.appendChild(label);
+    otherClientDiv.appendChild(button);
+  }
+};
+
+var performCall = function (otherEasyrtcid) {
+  easyrtc.hangupAll();
+
+  var successCB = function () {};
+  var failureCB = function () {};
+  easyrtc.call(otherEasyrtcid, successCB, failureCB);
+};
+
+var loginSuccess = function (easyrtcid) {
+  selfEasyrtcid = easyrtcid;
+  document.getElementById("iam").innerHTML = "My ID: " + easyrtc.cleanId(easyrtcid);
+};
+
+var loginFailure = function (errorCode, message) {
+  easyrtc.showError(errorCode, message);
+};
+
+$(function () {
+  $('#questions').hide();
+  connect();
+
+  $('#showquestions').click(function () {
+    $('#connectControls').toggle();
+    $('#showquestions').toggle();
+    $('#questions').toggle();
+  });
+
+  $('#hidequestions').click(function () {
+    $('#connectControls').toggle();
+    $('#showquestions').toggle();
+    $('#questions').toggle();
+  });
+});
